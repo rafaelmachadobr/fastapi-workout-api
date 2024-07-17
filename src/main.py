b383d0da4e13c24427d094191084a0f57d2623a8
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2024-07-16 21:34:40 -0300

    feat: Add Jaeger tracing support to the application
    The code changes include adding the necessary configuration in the `docker-compose.yml` file to include the Jaeger service. This enables distributed tracing for the application, allowing for monitoring and analysis of requests and performance.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,30 @@
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import Resource
 from src.routers import api_router
+
+trace.set_tracer_provider(
+    TracerProvider(resource=Resource.create({"service.name": "workout-api"}))
+)
+
+tracer_provider = trace.get_tracer_provider()
+
+jaeger_exporter = JaegerExporter(
+    agent_host_name="jaeger",
+    agent_port=6831,
+)
+
+tracer_provider.add_span_processor(BatchSpanProcessor(jaeger_exporter))
 
 app = FastAPI(title="Workout API")
 
 app.include_router(api_router)
 
 add_pagination(app)
+
+FastAPIInstrumentor.instrument_app(app)
